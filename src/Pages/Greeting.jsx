7675ce355af8aea47513b001cf012525d37e03f8
Lee145 <favournelson82@gmail.com>
2024-01-29 14:08:01 +0100

    Work in progress

--- src/Pages/Greeting.jsx
+++ src/Pages/Greeting.jsx
@@ -1,27 +1,39 @@
-import React, { useState, useEffect } from "react";
+// Greeting.js (Create a new component for the greeting)
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Greeting = () => {
-  const [timeOfDay, setTimeOfDay] = useState("");
+  const [greeting, setGreeting] = useState("");
+  const location = useLocation();
 
   useEffect(() => {
-    const determineTimeOfDay = () => {
-      const currentHour = new Date().getHours();
+    // Get current time and determine greeting based on hours
+    const currentTime = new Date().getHours();
+    let newGreeting = "";
 
-      if (currentHour >= 5 && currentHour < 12) {
-        setTimeOfDay("Good morning");
-      } else if (currentHour >= 12 && currentHour < 18) {
-        setTimeOfDay("Good afternoon");
-      } else {
-        setTimeOfDay("Good evening");
-      }
-    };
+    if (currentTime >= 5 && currentTime < 12) {
+      newGreeting = "Good morning,";
+    } else if (currentTime >= 12 && currentTime < 18) {
+      newGreeting = "Good afternoon,";
+    } else {
+      newGreeting = "Good evening,";
+    }
 
-    determineTimeOfDay();
-  }, []);
+    // Append additional greeting based on the page location
+    if (location.pathname === "/") {
+      newGreeting += " Welcome to my Home Section";
+    } else if (location.pathname === "/about") {
+      newGreeting += " Welcome to my About Section";
+    } else if (location.pathname === "/work") {
+      newGreeting += " Welcome to my Project Section";
+    }
+
+    setGreeting(newGreeting);
+  }, [location.pathname]);
 
   return (
     <div className="position-align">
-      <p className="timeOfDay">{timeOfDay}, welcome to my portfolio!</p>
+      <p className="timeOfDay">{greeting}</p>{" "}
     </div>
   );
 };
